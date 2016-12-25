7be4f1b62319389a991fd14a836b0c3c1c20dccf
Ahmed Ali <a.husseinzaki@gmail.com>
2016-12-25 01:21:12 -0800

    Add Empty Game Controller

---
+++ src/js/game-controller.js
@@ -0,0 +1,27 @@
+/*
+ * The Main GamePlay controller.
+ */
+class GameController extends BaseController {
+	/**
+	 * @param {Config} config - The game configuration. 
+	 */
+	constructor(config) {
+	}
+	/**
+	 * Gameplay Update method. It updates all the entities in the game.
+	 * It also checks the logic for losing.
+	 * @param {number} dt - The difference in seconds between last frame and next frame.
+	 * @param {ControllerChain} controllerChain - Updates the controller if necessary.
+	 */
+	update(dt, controllerChain) {
+		//TODO: Implement Update method.
+	}
+
+	/**
+	 * Gameplay Render method. It renders every entity in the game.
+	 * @param {Canvas} canvas - The Game Canvas.
+	 */
+	render(canvas) {
+		//TODO: Implement Render method.
+	}
+}
