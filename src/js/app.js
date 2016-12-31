68c9c86c80a1e4f04f1f2cf0d3090e48c17505b4
Ahmed Ali <a.husseinzaki@gmail.com>
2016-12-31 14:39:20 -0800

    Create App class as entry point

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * The main class for the game. It initializes the game engine, canvas and controllers.
+ */
+class App {
+	constructor() {
+		let width = document.body.clientWidth;
+		let height = document.body.clientHeight;
+		let config = new Config();
+		config.setCanvasWidth = width;
+		config.setCanvasHeight = height;
+		let canvas = new Canvas('game', width, height);
+		let entityGenerator = new EntityGenerator(config);
+		let mainController = new GameController(config, entityGenerator);
+		let controllerChain = new GameControllerChain(mainController);
+		let engine = new Engine(canvas, controllerChain);
+		engine.init();
+		engine.gameLoop();
+	}
+}
+
+window.addEventListener('load', () => new App());
